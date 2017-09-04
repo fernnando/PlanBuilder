f94b8bc0256335d14ac2c0cf23594530c963ac6a
fddittmar <fddittmar@gmail.com>
2017-09-04 01:58:18 -0400

    Created/Modified the 'NewProgramActivity' and its layout.

---
+++ app/src/main/java/co/fddittmar/planbuilder/NewProgramActivity.java
@@ -0,0 +1,13 @@
+package co.fddittmar.planbuilder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewProgramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_program);
+    }
+}
