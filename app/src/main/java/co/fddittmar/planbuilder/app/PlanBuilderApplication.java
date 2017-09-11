f2e5b49d7079312cc7d38e2c25d32931e44b278a
fddittmar <fddittmar@gmail.com>
2017-09-11 14:27:39 -0400

    Added the Dagger 2 library to the gradle file and created the packages 'app' and 'dagger' to setup and organize the files that we are gonna use for dependency injection.

---
+++ app/src/main/java/co/fddittmar/planbuilder/app/PlanBuilderApplication.java
@@ -0,0 +1,14 @@
+package co.fddittmar.planbuilder.app;
+
+import android.app.Application;
+
+/**
+ * Created by Fernnando on 11/09/2017.
+ */
+
+public class PlanBuilderApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
